7e1bdc122f053123c89a28257a59cddb4fe57694
PJ <yorko9999@gmail.com>
2015-03-07 15:00:03 +0000

    Added app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-var app = angular.module("countriesApp" , ['ngRoute']);
+var app = angular.module("countriesApp" , ['ngRoute'])
+
+.config(["$routeProvider", function($routeProvider){
+	
+$routeProvider
+	.when('/', {
+		templateUrl: 'templates/country_list.html',
+		controller: 'CountryListCtrl'
+	})
+	.when('/:countryName', {
+		templateUrl: "templates/country_detail.html",
+		controller: 'CountryDetailCtrl'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+}]);
