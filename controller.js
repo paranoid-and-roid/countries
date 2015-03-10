948b60e45e632b4d6a9fc8e571a75efae047b981
PJ <yorko9999@gmail.com>
2015-03-10 13:07:49 +0000

    Modified controller

--- controller.js
+++
@@ -1,9 +0,0 @@
-app.controller("CountriesCtrl", ["$scope", "$http", function($scope, $http) {
-
-	$http.get("countries.json").success(function(data) {
-		$scope.data = data;
-	});
-
-	$scope.sortField = 'population';
-
-}]);
